1f3d5d51ce29f91ebf7fc852fdcf5c43e9a87ff1
lillitarhea <lillitarhea@gmail.com>
2017-03-20 09:11:23 +0530

    Creates DB for user details

---
+++ app/src/main/java/com/example/android/dbtest/DataBasePractice.java
@@ -0,0 +1,216 @@
+package com.example.android.dbtest;
+
+import android.app.AlertDialog;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+public class DataBasePractice extends ActionBarActivity {
+    DatabaseHelper myDb;
+    EditText editName, editSurname, editMarks, editTextId;
+    Button btnAddData;
+    Button btnviewAll;
+    Button btnDelete;
+
+    Button btnviewUpdate;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_base_practice);
+         //SQLiteDatabase myDb = new DatabaseHelper(this).getWritableDatabase();
+        myDb = new DatabaseHelper(this);
+
+        editName = (EditText) findViewById(R.id.editText_name);
+        editSurname = (EditText) findViewById(R.id.editText_surname);
+        editMarks = (EditText) findViewById(R.id.editText_Marks);
+        editTextId = (EditText) findViewById(R.id.editText_id);
+        btnAddData = (Button) findViewById(R.id.button_add);
+        btnviewAll = (Button) findViewById(R.id.button_viewAll);
+        btnviewUpdate = (Button) findViewById(R.id.button_update);
+        btnDelete = (Button) findViewById(R.id.button_delete);
+        AddData();
+        viewAll();
+        UpdateData();
+        DeleteData();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    public void DeleteData() {
+        btnDelete.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Integer deletedRows = myDb.deleteData(editTextId.getText().toString());
+                        if (deletedRows > 0)
+                            Toast.makeText(DataBasePractice.this, "Data Deleted", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(DataBasePractice.this, "Data not Deleted", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+
+    public void UpdateData() {
+        btnviewUpdate.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isUpdate = myDb.updateData(editTextId.getText().toString(),
+                                editName.getText().toString(),
+                                editSurname.getText().toString(), editMarks.getText().toString());
+                        if (isUpdate == true)
+                            Toast.makeText(DataBasePractice.this, "Data Update", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(DataBasePractice.this, "Data not Updated", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+
+    public void AddData() {
+        btnAddData.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = myDb.insertData(editName.getText().toString(),
+                                editSurname.getText().toString(),
+                                editMarks.getText().toString());
+                        if (isInserted == true)
+                            Toast.makeText(DataBasePractice.this, "Data Inserted", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(DataBasePractice.this, "Data not Inserted", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+
+    public void viewAll() {
+        btnviewAll.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Cursor res = myDb.getAllData();
+                        if (res.getCount() == 0) {
+                            // show message
+                            showMessage("Error", "Nothing found");
+                            return;
+                        }
+
+                        StringBuffer buffer = new StringBuffer();
+                        while (res.moveToNext()) {
+                            buffer.append("Id :" + res.getString(0) + "\n");
+                            buffer.append("Name :" + res.getString(1) + "\n");
+                            buffer.append("Surname :" + res.getString(2) + "\n");
+                            buffer.append("Marks :" + res.getString(3) + "\n\n");
+                        }
+
+                        // Show all data
+                        showMessage("Data", buffer.toString());
+                    }
+                }
+        );
+    }
+
+    public void showMessage(String title, String Message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_data_base_practice, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.android.dbtest/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.android.dbtest/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+
+
+}
